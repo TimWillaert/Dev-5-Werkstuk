9af44e7857e03fd611075010964eb7ed8236ed47
Tim Willaert <tim.willaert@student.ehb.be>
2021-01-02 13:07:20 +0100

    develop(database): prepared database structure

--- api/src/server.js
+++ api/src/server.js
@@ -2,8 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const http = require("http");
 const Helpers = require("./utils/helpers.js");
-
-const port = 3000;
+const DatabaseHelper = require("./utils/DatabaseHelper.js");
 
 const pg = require("knex")({
   client: "pg",
@@ -81,45 +80,6 @@ app.delete("/deleteStory", (req, res) => {
   //   });
 });
 
-async function initialiseTables() {
-  await pg.schema.hasTable("storyblock").then(async (exists) => {
-    if (!exists) {
-      await pg.schema
-        .createTable("storyblock", (table) => {
-          table.increments();
-          table.uuid("uuid");
-          table.string("content");
-          table.string("story_id");
-          table.integer("order");
-          table.timestamps(true, true);
-        })
-        .then(async () => {
-          console.log("created table storyblock");
-        });
-    }
-  });
-  await pg.schema.hasTable("story").then(async (exists) => {
-    if (!exists) {
-      await pg.schema
-        .createTable("story", (table) => {
-          table.increments();
-          table.uuid("uuid");
-          table.string("title");
-          table.string("summary");
-          table.timestamps(true, true);
-        })
-        .then(async () => {
-          console.log("created table story");
-          for (let i = 0; i < 10; i++) {
-            const uuid = Helpers.generateUUID();
-            await pg
-              .table("story")
-              .insert({ uuid, title: `random element number ${i}` });
-          }
-        });
-    }
-  });
-}
-initialiseTables();
+DatabaseHelper.initialiseTables();
 
 module.exports = app;
---
+++ api/src/utils/DatabaseHelper.js
@@ -0,0 +1,54 @@
+const pg = require("knex")({
+    client: "pg",
+    version: "9.6",
+    searchPath: ["knex", "public"],
+    connection: process.env.PG_CONNECTION_STRING
+      ? process.env.PG_CONNECTION_STRING
+      : "postgres://example:example@localhost:5432/test",
+  });
+
+const DatabaseHelper = {
+    async initialiseTables() {
+        await pg.schema.hasTable("tickets").then(async (exists) => {
+          if (!exists) {
+            await pg.schema
+              .createTable("tickets", (table) => {
+                table.increments();
+                table.uuid("uuid");
+                table.string("summary");
+                table.json("requirements");
+                table.string("assigner");
+                table.string("assignee");
+                table.string("deadline");
+                table.string("organisation_id");
+                table.timestamps(true, true);
+              })
+              .then(async () => {
+                console.log("created table tickets");
+              });
+          }
+        });
+        await pg.schema.hasTable("organisations").then(async (exists) => {
+          if (!exists) {
+            await pg.schema
+              .createTable("organisations", (table) => {
+                table.increments();
+                table.uuid("uuid");
+                table.string("name");
+                table.timestamps(true, true);
+              })
+              .then(async () => {
+                console.log("created table organisations");
+                for (let i = 0; i < 10; i++) {
+                  const uuid = Helpers.generateUUID();
+                  await pg
+                    .table("organisations")
+                    .insert({ uuid, name: `Organisation ${i}`});
+                }
+              });
+          }
+        });
+    }
+}
+
+module.exports = DatabaseHelper, pg;
